59b342cb6dd25cf756d81944592ba5dec0146d42
hmyo2853 <35989487+hmyo2853@users.noreply.github.com>
2022-12-28 00:00:29 +0900

    modal 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer () {
   return(
     <div className={styles.Footer}>
       <div className={styles.Bar}></div>
-      <div style={{margin: "2rem 0 4rem 0"}}><a href="" target="_blank">GITHUB</a>|<a href="" target="_blank">EMAIL</a>|<a href="mailto:hmyo2853@gmail.com" style={{cursor: "pointer"}}>문의하기</a></div>
+      <div style={{margin: "2rem 0 4rem 0"}}><a href="https://github.com/hmyo2853/HowAreNews" target="_blank">GITHUB</a>|<a href="https://generated-silence-3bf.notion.site/FE-db5937393afa4fd1804f545bbc4e6913" target="_blank">NOTION</a>|<a href="mailto:hmyo2853@gmail.com" style={{cursor: "pointer"}}>문의하기</a></div>
       <div style={{margin: "2rem 0 4rem 0"}}>Copyright all right deserved @hmyo2853</div>
     </div>
   )
