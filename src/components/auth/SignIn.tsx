9b3654235e9ea58be6ceacf6a0d9f65255066ec2
hmyo2853 <hmyo2853@gmail.com>
2023-01-27 23:45:32 +0900

    refactor : Auth -> Signin, Signup 컴포넌트로 분리

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+
+/** 로그인 컴포넌트 */
 const SignIn = () => {
-  return <>회원가입 페이지</>;
+  const [email, setEmail] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [pwd, setPwd] = useState<string>("");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target.id;
+    const value = e.target.value;
+    // email input change
+    if (target === "email") {
+      // 특수문자 @._-  + 알파벳만 허용
+      setEmail(value);
+    } else if (target === "password") {
+      // password input change
+      setPwd(value);
+    } else if (target === "name") {
+      setName(value);
+    }
+  };
+  return (
+    <>
+      <h2>로그인</h2>
+      <input
+        type="email"
+        placeholder="이메일 *"
+        id="email"
+        onChange={onChange}
+        required
+      />
+      <input
+        type="password"
+        placeholder="비밀번호 *"
+        id="password"
+        onChange={onChange}
+        required
+      />
+    </>
+  );
 };
 
 export default SignIn;
--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,5 +1,52 @@
+import { useState } from "react";
+
+/** 회원가입 컴포넌트 */
 const SignUp = () => {
-  return <>회원가입 페이지</>;
+  const [email, setEmail] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [pwd, setPwd] = useState<string>("");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target.id;
+    const value = e.target.value;
+    // email input change
+    if (target === "email") {
+      // 특수문자 @._-  + 알파벳만 허용
+      setEmail(value);
+    } else if (target === "password") {
+      // password input change
+      setPwd(value);
+    } else if (target === "name") {
+      setName(value);
+    }
+  };
+
+  return (
+    <>
+      <h2>회원가입</h2>
+      <input
+        type="text"
+        placeholder="이름 *"
+        id="name"
+        onChange={onChange}
+        required
+      />
+      <input
+        type="email"
+        placeholder="이메일 *"
+        id="email"
+        onChange={onChange}
+        required
+      />
+      <input
+        type="password"
+        placeholder="비밀번호 *"
+        id="password"
+        onChange={onChange}
+        required
+      />
+    </>
+  );
 };
 
 export default SignUp;
